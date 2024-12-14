00ce75da488613e7c113c8f02d759f416e6c8533
Roverp <yar.zubaha@proton.me>
2024-12-14 12:05:51 +0100

    Add header

---
+++ src/components/header/header.jsx
@@ -0,0 +1,22 @@
+import githubLogo from "../../assets/img/header/github-logo.svg"
+
+import "./header.css";
+
+const Header = () => {
+  return (
+    <>
+      <header className="header">
+        <div className="container header__content">
+          <div className="header__logo">
+            Word to the People
+          </div>
+          <nav>
+            <a href="https://github.com/Roverp1/Word_To_The_People.jsx/" target="_blank"><img src={githubLogo} alt="link to github"/></a>
+          </nav>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
